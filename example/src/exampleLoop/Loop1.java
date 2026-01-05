44c59180f2d77108a514fef925c82b9b2f3e79f9
DW-203 <DW-203@DESKTOP-RM80K3L>
2026-01-05 19:22:16 +0900

    1월 5일 수업 후 과제풀이

---
+++ example/src/exampleLoop/Loop1.java
@@ -0,0 +1,52 @@
+package exampleLoop;
+
+import java.util.Scanner;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		// 	1월 5일 과제
+		
+		// 가위 바위 보 게임을 총 15판 진행 > for문으로 15회 반복, if문으로 승, 패, 무 구분
+		// 게임을 진행 하면서 1판 끝날 때 마다
+		// 몇승, 몇패, 몇무 출력하기 > count1, count2, count3로 처리
+		// 가위 ==1 / 바위 == 2 / 보 == 3
+		
+		int count1 = 0; // 승리 카운트
+		int count2 = 0; // 패배 카운트
+		int count3 = 0; // 무승부 카운트
+		int count4 = 0; // 총횟수 카운트
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		for(int i =1;i<=15;i++) { // 15회 반복
+			System.out.print("안 내면 술래 가위바위보~ (가위: 1, 바위: 2, 보: 3) : ");
+			// 컴퓨터와 user의 가위바위보 정하기
+			int com = (int)Math.floor(Math.random()*3)+1;
+			int user = sc.nextInt();
+			
+			//조건문 승리, 패배, 무승부
+			if((user ==1 &&  com==3)||(user == 2&& com == 1)||(user==3 && com==2) ) {
+				count1++;
+				System.out.print("승리 : "+count1+"회 / ");
+			}else if((com ==1 &&  user==3)||(com == 2&& user == 1)||(com==3 && user==2)) {
+				count2++;
+				System.out.print("패배 : "+count2+"회 / ");
+			}else {
+				if(user > 3) {
+					System.out.println("숫자를 바로 입력하시오");
+				}else {
+					count3++;
+					System.out.print("무승부 :"+count3+"회 / ");
+				}
+			}
+			// 총횟수 카운트
+			count4++;
+			System.out.println("총횟수 : "+count4+"회");
+		}
+System.out.println("---<게임 종료>--- \n 승리 : "+count1+"회 \n 패배 : "+count2+"회 \n 무승부 :"+count3+"회");
+	}
+
+}
---
+++ example/src/exampleLoop/Loop2.java
@@ -0,0 +1,57 @@
+package exampleLoop;
+
+import java.util.Scanner;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1월 5일 과제2
+		 * 
+		 * 업다운 게임. 내가 숫자를 제시하면 컴퓨터가 맞추도록 설계. 랜덤 범위를 업 다운으로 줄이도록 설계 범위 1~50 내가 37입력 컴퓨터
+		 * 25제시 내가 UP, 범위 26~50으로 변경 컴퓨터 40제시 내가 DOWN, 범위 26~39으로 변경 컴퓨터 31제시 내가 UP, 범위
+		 * 32~39으로 변경
+		 */
+		
+		// 최댓값과 최솟값 변수
+		int max = 50;
+		int min = 0;
+		
+		// 숫자 입력
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("1~50 중 입력 : ");
+		int user = scan.nextInt();
+		
+		// 컴퓨터 숫자 고르기
+		int com = (int) Math.floor(Math.random() * 50) + 1;
+		System.out.println("컴퓨터가 고른 숫자 : " + com);
+
+		
+		// 반복문과 조건문
+		// 반복문 컴퓨터가 정답을 맞출 때까지 반복
+		// 조건문 만약 컴퓨터가 제시한 숫자가 정답보다 낮다면 UP, 높다면 DOWN 출력
+		// 조건문 UP, DOWN 반영해서 다시 범위 지정
+		while (true) {
+
+			if (com < user) { // 난수가 정답보다 작은 경우 /ex) 난수 : 35, 정답 : 40
+				System.out.println("UP");
+				min = com;
+				com = (int) Math.floor(Math.random() * (max - min + 1)) + min;
+
+				System.out.println("컴퓨터가 고른 숫자 : " + com);
+			} else if (com > user) { // 난수가 정답보다 큰 경우 /ex) 난수 : 35, 정답 : 25
+				System.out.println("DOWN");
+				max = com;
+				com = (int) Math.floor(Math.random() * (max - min + 1)) + min;
+				System.out.println("컴퓨터가 고른 숫자 : " + com);
+			} else if (user == com) {
+				System.out.println(" 정답 !");
+				break;
+			} else if (com == max || com == min) {
+				continue;
+			}
+
+		}
+	}
+}
