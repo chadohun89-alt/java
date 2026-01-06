42bb9347b52cd51c13231dd54385cd57993d30bc
DW-203 <DW-203@DESKTOP-RM80K3L>
2026-01-06 14:33:31 +0900

    실험

--- java_control/src/study20260106/ArrayMain.java
+++ java_control/src/study20260106/ArrayMain.java
@@ -4,14 +4,50 @@ public class ArrayMain {
 
 	public static void main(String[] args) {
 		
-		int [] arr = new int [5];
-		// arr은 참조 변수이다. 참조 변수는 메모리 주소를 저장할 수 있는 변수이다.
-		//  
+		String a;
+		int b;
 		
-		System.out.println(arr);
-		int [] arr2 = {10, 20, 30};
+		char c = 'a';
+		char[] temp = new char[5];
+		
+		temp[0] = 'g'; temp[1] = 'o'; temp[2] = 'o'; temp[3] = 'd';
+		System.out.println(temp);
 		
 		
+		
+		
+		
+		
+		
+		
+//		int[] arr = new int[5];
+//		// arr은 참조 변수이다. 참조 변수는 메모리 주소를 저장할 수 있는 변수이다.
+//		//
+//
+//		System.out.println(arr);
+//		System.out.println("-----------");
+//
+//		arr[0] = 10;
+//		arr[1] = 20;
+//		arr[2] = 55;
+//		arr[3] = 11;
+//		arr[4] = 99;
+//
+//		System.out.println(arr[2]);
+//		System.out.println("-----------");
+//
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
+//
+//		System.out.println("----------");
+//
+//		for (int num : arr) { // 향상된 for 문
+//			if (num > 50)
+//				System.out.println(num);
+//		}
+//
+//		int[] arr2 = { 10, 20, 30 };
 
 	}
 
