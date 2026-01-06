0b2c9067670fc28b3768b34c75683e4ef162dbd4
DW-203 <DW-203@DESKTOP-RM80K3L>
2026-01-06 13:45:29 +0900

    1월 6일 수업 중(배열1)

--- java_control/src/study20260106/ArrayMain.java
+++ java_control/src/study20260106/ArrayMain.java
@@ -3,8 +3,22 @@ package study20260106;
 public class ArrayMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int [] arr = new int [5];
+		// arr은 참조 변수이다. 참조 변수는 메모리 주소를 저장할 수 있는 변수이다.
+		//  
+		
+		System.out.println(arr);
+		int [] arr2 = {10, 20, 30};
+		
+		
 
 	}
 
 }
+/*
+ * 배열 - 동일한 데이터 타입의 데이터 저장공간이 연속적으로 나열되어 있는 구조
+ * 
+ * 
+ * 
+ */
